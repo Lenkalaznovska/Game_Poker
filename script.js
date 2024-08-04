8d0e50dd0f1885e173e4a63175b2c3ba017b5d04
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-08-04 18:37:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,108 @@
 let cardImages = {
-  "2-C": "https://deckofcardsapi.com/static/img/2C.png",
-  "2-D": "https://deckofcardsapi.com/static/img/2D.png",
-  "2-H": "https://deckofcardsapi.com/static/img/2H.png",
-  "2-S": "https://deckofcardsapi.com/static/img/2S.png",
-  "3-C": "https://deckofcardsapi.com/static/img/3C.png",
-  "3-D": "https://deckofcardsapi.com/static/img/3D.png",
-  "3-H": "https://deckofcardsapi.com/static/img/3H.png",
-  "3-S": "https://deckofcardsapi.com/static/img/3S.png",
-  "4-C": "https://deckofcardsapi.com/static/img/4C.png",
-  "4-D": "https://deckofcardsapi.com/static/img/4D.png",
-  "4-H": "https://deckofcardsapi.com/static/img/4H.png",
-  "4-S": "https://deckofcardsapi.com/static/img/4S.png",
-  "5-C": "https://deckofcardsapi.com/static/img/5C.png",
-  "5-D": "https://deckofcardsapi.com/static/img/5D.png",
-  "5-H": "https://deckofcardsapi.com/static/img/5H.png",
-  "5-S": "https://deckofcardsapi.com/static/img/5S.png",
-  "6-C": "https://deckofcardsapi.com/static/img/6C.png",
-  "6-D": "https://deckofcardsapi.com/static/img/6D.png",
-  "6-H": "https://deckofcardsapi.com/static/img/6H.png",
-  "6-S": "https://deckofcardsapi.com/static/img/6S.png",
-  "7-C": "https://deckofcardsapi.com/static/img/7C.png",
-  "7-D": "https://deckofcardsapi.com/static/img/7D.png",
-  "7-H": "https://deckofcardsapi.com/static/img/7H.png",
-  "7-S": "https://deckofcardsapi.com/static/img/7S.png",
-  "8-C": "https://deckofcardsapi.com/static/img/8C.png",
-  "8-D": "https://deckofcardsapi.com/static/img/8D.png",
-  "8-H": "https://deckofcardsapi.com/static/img/8H.png",
-  "8-S": "https://deckofcardsapi.com/static/img/8S.png",
-  "9-C": "https://deckofcardsapi.com/static/img/9C.png",
-  "9-D": "https://deckofcardsapi.com/static/img/9D.png",
-  "9-H": "https://deckofcardsapi.com/static/img/9H.png",
-  "9-S": "https://deckofcardsapi.com/static/img/9S.png",
-  "10-C": "https://deckofcardsapi.com/static/img/0C.png",
-  "10-D": "https://deckofcardsapi.com/static/img/0D.png",
-  "10-H": "https://deckofcardsapi.com/static/img/0H.png",
-  "10-S": "https://deckofcardsapi.com/static/img/0S.png",
-  "J-C": "https://deckofcardsapi.com/static/img/JC.png",
-  "J-D": "https://deckofcardsapi.com/static/img/JD.png",
-  "J-H": "https://deckofcardsapi.com/static/img/JH.png",
-  "J-S": "https://deckofcardsapi.com/static/img/JS.png",
-  "Q-C": "https://deckofcardsapi.com/static/img/QC.png",
-  "Q-D": "https://deckofcardsapi.com/static/img/QD.png",
-  "Q-H": "https://deckofcardsapi.com/static/img/QH.png",
-  "Q-S": "https://deckofcardsapi.com/static/img/QS.png",
-  "K-C": "https://deckofcardsapi.com/static/img/KC.png",
-  "K-D": "https://deckofcardsapi.com/static/img/KD.png",
-  "K-H": "https://deckofcardsapi.com/static/img/KH.png",
-  "K-S": "https://deckofcardsapi.com/static/img/KS.png",
-  "A-C": "https://deckofcardsapi.com/static/img/AC.png",
-  "A-D": "https://deckofcardsapi.com/static/img/AD.png",
-  "A-H": "https://deckofcardsapi.com/static/img/AH.png",
-  "A-S": "https://deckofcardsapi.com/static/img/AS.png",
+  "2-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/2_of_clubs.png",
+  "2-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/2_of_diamonds.png",
+  "2-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/2_of_hearts.png",
+  "2-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/2_of_spades.png",
+  "3-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/3_of_clubs.png",
+  "3-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/3_of_diamonds.png",
+  "3-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/3_of_hearts.png",
+  "3-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/3_of_spades.png",
+  "4-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/4_of_clubs.png",
+  "4-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/4_of_diamonds.png",
+  "4-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/4_of_hearts.png",
+  "4-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/4_of_spades.png",
+  "5-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/5_of_clubs.png",
+  "5-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/5_of_diamonds.png",
+  "5-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/5_of_hearts.png",
+  "5-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/5_of_spades.png",
+  "6-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/6_of_clubs.png",
+  "6-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/6_of_diamonds.png",
+  "6-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/6_of_hearts.png",
+  "6-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/6_of_spades.png",
+  "7-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/7_of_clubs.png",
+  "7-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/7_of_diamonds.png",
+  "7-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/7_of_hearts.png",
+  "7-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/7_of_spades.png",
+  "8-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/8_of_clubs.png",
+  "8-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/8_of_diamonds.png",
+  "8-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/8_of_hearts.png",
+  "8-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/8_of_spades.png",
+  "9-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/9_of_clubs.png",
+  "9-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/9_of_diamonds.png",
+  "9-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/9_of_hearts.png",
+  "9-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/9_of_spades.png",
+  "10-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/10_of_clubs.png",
+  "10-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/10_of_diamonds.png",
+  "10-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/10_of_hearts.png",
+  "10-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/10_of_spades.png",
+  "J-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/jack_of_clubs.png",
+  "J-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/jack_of_diamonds.png",
+  "J-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/jack_of_hearts.png",
+  "J-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/jack_of_spades.png",
+  "Q-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/queen_of_clubs.png",
+  "Q-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/queen_of_diamonds.png",
+  "Q-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/queen_of_hearts.png",
+  "Q-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/queen_of_spades.png",
+  "K-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/king_of_clubs.png",
+  "K-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/king_of_diamonds.png",
+  "K-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/king_of_hearts.png",
+  "K-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/king_of_spades.png",
+  "A-C":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/ace_of_clubs.png",
+  "A-D":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/ace_of_diamonds.png",
+  "A-H":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/ace_of_hearts.png",
+  "A-S":
+    "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/ace_of_spades.png",
 };
 
 function getCardImageUrl(card) {
@@ -62,9 +114,10 @@ function displayCard(card, containerClass) {
   let imageUrl = getCardImageUrl(card);
   cardImg.src = imageUrl;
   cardImg.onerror = function () {
-    this.src = "https://deckofcardsapi.com/static/img/blue_back.png";
+    this.src =
+      "https://raw.githubusercontent.com/hayeah/playing-cards-assets/master/png/back.png";
   };
-  document.querySelector(.${containerClass}).appendChild(cardImg);
+  document.querySelector(`.${containerClass}`).appendChild(cardImg);
 }
 
 function evaluateHand(hand) {
@@ -82,12 +135,26 @@ let playerBalance = 1000;
 
 function buildDeck() {
   const suits = ["C", "D", "H", "S"];
-  const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
   deck = [];
 
   for (let suit of suits) {
     for (let value of values) {
-      deck.push(${value}-${suit});
+      deck.push(`${value}-${suit}`);
     }
   }
 }
@@ -166,42 +233,11 @@ function dealCards() {
 function showMessage(message, type) {
   let resultsDiv = document.querySelector(".results");
   resultsDiv.innerText = message;
-  resultsDiv.className = results ${type};
+  resultsDiv.className = `results ${type}`;
   resultsDiv.style.display = "block";
 }
 
 document.querySelector(".deal").addEventListener("click", function () {
   document.querySelector(".results").style.display = "none";
   dealCards();
-});                      <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Poker Game</title>
-    <link rel="stylesheet" href="css/style.css" />
-  </head>
-  <body>
-    <div class="game-container">
-      <div class="dealer">
-        <h2>Community Cards</h2>
-        <div class="community-cards"></div>
-      </div>
-      <div class="player">
-        <h2>Your Hand</h2>
-        <div class="player-cards"></div>
-      </div>
-      <div class="account">
-        <h2>Account Balance: <span class="balance">1000</span> Chips</h2>
-      </div>
-      <div class="controls">
-        <button class="deal">Deal</button>
-      </div>
-      <div class="results">
-        <!-- Results will be shown here -->
-      </div>
-    </div>
-
-    <script src="script.js" defer></script>
-  </body>
-</html>
+});
